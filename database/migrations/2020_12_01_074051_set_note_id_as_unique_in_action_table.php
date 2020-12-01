3a4890055ccb61351359db945e11998109c1fa8f
Fajar AM <somabangsa@gmail.com>
2020-12-01 07:42:07 +0700

    add migration set note id unique

---
+++ database/migrations/2020_12_01_074051_set_note_id_as_unique_in_action_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class SetNoteIdAsUniqueInActionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('action', function (Blueprint $table) {
+            DB::statement('ALTER TABLE kafila_console.actions ADD CONSTRAINT actions_un UNIQUE KEY (note_id)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('action', function (Blueprint $table) {
+            //
+        });
+    }
+}
